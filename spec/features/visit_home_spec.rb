5172767af44415ffdda641be7923a23fd10e3006
matt-smrke <matthew.smrke@empire.ca>
2017-08-29 12:11:22 -0400

    home page spec

---
+++ spec/features/visit_home_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Visiting the Home Page" do
+  scenario "A user visits the home page" do
+    visit "/"
+
+    expect(page).to have_link('Athletes Den')
+    expect(page).to have_link('Home')
+    expect(page).to have_content('Workout Lounge!')
+    expect(page).to have_content('Show off your workout')
+  end
+end
